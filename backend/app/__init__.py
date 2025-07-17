ee35174c0ad53745e43b22c627a7b062f5c0f868
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-07-18 00:49:47 +0300

    commit flask set up,db config and migrations

---
+++ backend/app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app():
+    load_dotenv()
+    app = Flask(__name__,instance_relative_config=True)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    CORS(app)
+    JWTManager(app)
+
+   
+
+    return app
