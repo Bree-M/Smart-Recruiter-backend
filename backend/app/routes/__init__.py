b763433e2ffe3f7019921ad77c6ddd166597e337
Ahmed <ahmedbwire17@gmail.com>
2025-07-21 09:16:22 +0300

    register codewars

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -2,8 +2,10 @@ from app.routes.auth_routes import auth_bp
 from app.routes.assessment_routes import assessment_bp
 from app.routes.recruiter_routes import recruiter_bp
 from app.routes.interviewee_routes import interviewee_bp
+from codewars_routes import codewars_routes
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(assessment_bp)
 app.register_blueprint(recruiter_bp)
 app.register_blueprint(interviewee_bp)
+app.register_blueprint(codewars_routes)
