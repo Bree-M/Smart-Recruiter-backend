1d35d3db9b3ad73c4b7c49f6551d9bd35d6ee4f5
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-07-30 16:26:34 +0300

    feedback route added

--- backend/app/routes/recruiter_routes.py
+++ backend/app/routes/recruiter_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from backend.app.models import db, Invitation, Response,Result,Submission,Assessment
+from backend.app.models import db, Invitation, Response,Result,Submission,Assessment,Feedback
 
 recruiter_bp = Blueprint('recruiter', __name__, url_prefix='/recruiter')
 
@@ -82,4 +82,66 @@ def get_submission(submission_id):
     if not assessment:
         return jsonify({'error':'Cannot Grant Access!'}),403
     return jsonify(submission.serialize()),200
+
+
+@recruiter_bp.route('/feedback',methods=['POST'])
+@jwt_required()
+def add_feedback():
+    user=get_jwt_identity()
+    if user['role'] != 'recruiter':
+        return jsonify({'error':'Only Recruiters Can Give Feedback!'}),403
+    
+    data=request.get_json()
+    required=['submission_id','response_id','comment']
+    if not all(k in data for k in required):
+        return jsonify({'error':'Fields Required!'}),400
+    
+    feedback=Feedback(
+        submission_id=data['submission_id'],
+        response_id=data['response_id'],
+        comment=data['comment'],
+        recruiter_id=user['id']
+
+    )
+    db.session.add(feedback)
+    db.session.commit()
+
+    return jsonify({'messsage':'Feedback Added!','feedback':feedback.serialize()}),201
+
+
+@recruiter_bp.route('/feedback/<int:feedback_id>',methods=['PATCH'])
+@jwt_required()
+def update_feedback(feedback_id):
+    user=get_jwt_identity()
+    feedback=Feedback.query.get(feedback_id)
+    if not feedback:
+        return jsonify({'error':'Feedback Not Found!'}),404
+    if feedback.recruiter_id != user['id']:
+        return jsonify({'error':'Authorization Required!'}),403
+    
+    data=request.get_json()
+    if 'comment' in data:
+        feedback.comment=data['comment']
+
+    db.session.commit() 
+    return jsonify({'message':'Feedback Updated','feedback':feedback.serialize()}),200
+
+
+@recruiter_bp.route('/feedback/<int:feedback_id>',methods=['DELETE'])
+@jwt_required()
+def delete_feedback(feedback_id):
+    user=get_jwt_identity()
+    feedback=Feedback.query.get(feedback_id)
+    if not feedback:
+        return jsonify({'error':'Feedback Not Found!'}),404
+    if feedback.recruiter_id != user['id']:
+        return jsonify({'error':'Authorization Required!'}),403
+    
+    db.session.delete(feedback)
+    db.session.commit()
+    return jsonify({'message':'Feedback Deleted'}),200
+    
+
+
+
 
