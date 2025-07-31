6147a2fd458c6ed2cf036020d67c98c57715dfcd
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-08-01 01:47:31 +0300

    test for deployment

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,4 +1,4 @@
-from backend.app import create_app
+from flask import Flask,jsonify
 from flask_cors import CORS
 
 def create_app():
@@ -7,9 +7,12 @@ def create_app():
 
     @app.route('/api/recruiters',methods=['GET'])
     def get_recruiters():
-        return [{"id":1,"name":"Test Recruiter"}],200
+        return jsonify([{"id":1,"name":"Test Recruiter"},{"id":2,"name":"Recruiter Test"}]),200
     
     return app
 app=create_app()
+
+if __name__=="__main__":
+    app.run(debug=True)
 
 
